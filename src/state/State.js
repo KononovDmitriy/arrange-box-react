2a73ed6a30fad1eb581ca58119e15f0b0b573e42
Dmitriy Kononov <ds.kononov@mail.ru>
2021-05-28 11:48:03 +0300

    Создал временный State

---
+++ src/state/State.js
@@ -0,0 +1,23 @@
+export const box1 = [{
+  id: 111,
+  label: 'Карточка 1',
+  selected: false
+},{
+  id: 112,
+  label: 'Карточка 2',
+  selected: false
+},{
+  id: 113,
+  label: 'Карточка 3',
+  selected: false
+},{
+  id: 114,
+  label: 'Карточка 4',
+  selected: false
+},{
+  id: 111,
+  label: 'Карточка 5',
+  selected: false
+}];
+
+export const box2 = [];
